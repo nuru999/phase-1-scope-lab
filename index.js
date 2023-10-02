1553844be9ac50bdf5f837da0fadc5e7199a910c
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-10-02 11:23:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    if (customerName) {
+      customerName = customerName.toUpperCase();
+    }
+  }
+  
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+ 
+  const leastFavoriteCustomer = 'someone';
+
+  function changeLeastFavoriteCustomer() {
+    // Attempting to change the constant will result in an error
+    leastFavoriteCustomer = 'new least favorite'; // This line will throw an error
+  }
+  
